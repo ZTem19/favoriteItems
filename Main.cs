36f3e102e86eb69e17435e1bf9a8507b80694c60
Zander <zander.templin@gmail.com>
2026-02-20 18:50:52 -0600

    Updated version number and edited .gitignore

--- Main.cs
+++ Main.cs
@@ -12,6 +12,7 @@ using System.Reflection;
 using System.Collections;
 using System.Drawing;
 using TMPro;
+using BepInEx.Configuration;
 
 
 namespace favoriteItems
@@ -22,7 +23,7 @@ namespace favoriteItems
         // BepIn Setup
         const string pluginGUID = "com.grizzyggs.favoriteItems";
         const string pluginName = "favorite Items";
-        const string pluginVersion = "0.0.2";
+        const string pluginVersion = "0.1.1";
 
         private readonly Harmony HarmonyInstance = new Harmony(pluginGUID);
 
@@ -35,17 +36,26 @@ namespace favoriteItems
         public static ItemDrop.ItemData CurrentHoveredItem = null;
         // End Custom Variables
 
+        // Config File 
+        private ConfigEntry<bool> modEnabled;
+        private static ConfigEntry<KeyCode> favoriteKey;
+
 
 
         public void Awake()
-        {
-            Main.logger.LogInfo("Thank you for using my mod!");
+        { 
+            modEnabled = Config.Bind("General", "Enabled", true, "Enable or disable the Favorite Items mod.");
+            favoriteKey = Config.Bind("Settings", "FavoriteHotkey", KeyCode.LeftAlt, "Hotkey to toggle favorite status on hovered item.");
 
-
-
-
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            HarmonyInstance.PatchAll(assembly);
+            if (!modEnabled.Value) {
+                Main.logger.LogInfo("Favorite Items mod is disabled in config. Exiting.");
+                return;
+            }
+            else {
+                Main.logger.LogInfo("Loaded Favorite Items with config file");
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                HarmonyInstance.PatchAll(assembly);
+            }
         }
 
         void FixedUpdate()
@@ -56,7 +66,7 @@ namespace favoriteItems
         void Update()
         {
             // Simple Hotkey to toggle "Favorite" status on hovered item
-            if (UnityEngine.Input.GetKeyDown(KeyCode.F) && Main.CurrentHoveredItem != null)
+            if (UnityEngine.Input.GetKeyDown(Main.favoriteKey.Value) && Main.CurrentHoveredItem != null)
             {
                 Main.logger.LogDebug($"Attempting to toggle favorite status on item: {Main.CurrentHoveredItem}");
                 if (Main.CurrentHoveredItem.m_customData.ContainsKey(FavoriteKey))
@@ -79,19 +89,29 @@ namespace favoriteItems
         {
             if (InventoryGui.instance != null)
             {
-                InventoryGrid playerGrid = InventoryGui.instance.m_playerGrid;
-                InventoryGrid.Element hoveredElement = playerGrid.GetHoveredElement();
-                if (hoveredElement != null)
-                {
-                    ItemDrop.ItemData hoveredItem = playerGrid.m_inventory.GetItemAt(hoveredElement.m_pos.x, hoveredElement.m_pos.y);
-                    Main.CurrentHoveredItem = hoveredItem;
-                    Main.logger.LogDebug($"hovereditem: {hoveredItem.m_shared.m_name}");
-                } else
-                {
-                    Main.logger.LogDebug("hoveredelement is null");
-                }
-
+                Main.logger.LogDebug("inventory is null");
+                return; 
             }
+            
+            InventoryGrid playerGrid = InventoryGui.instance.m_playerGrid;
+            if (playerGrid == null)
+            {
+                Main.logger.LogDebug("playergrid is null");
+                return;
+            }
+
+            InventoryGrid.Element hoveredElement = playerGrid.GetHoveredElement();
+            if (hoveredElement != null)
+            {
+                ItemDrop.ItemData hoveredItem = playerGrid.m_inventory.GetItemAt(hoveredElement.m_pos.x, hoveredElement.m_pos.y);
+                Main.CurrentHoveredItem = hoveredItem;
+                Main.logger.LogDebug($"hovereditem: {hoveredItem.m_shared.m_name}");
+            }
+            else
+            {
+                Main.logger.LogDebug("hoveredelement is null");
+            }
+
         }
 
         [HarmonyPatch(typeof(Humanoid), nameof(Player.IsItemEquiped))]
